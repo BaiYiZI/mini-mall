9c93bdcb3ea21d38f8e33d86e3fb23aa2be7f130
Zhangbokai614 <64344118+Zhangbokai614@users.noreply.github.com>
2022-09-17 23:39:49 +0800

    feat: Add page bottom tab

--- src/app.config.js
+++ src/app.config.js
@@ -1,18 +1,44 @@
 export default defineAppConfig({
   pages: [
-    'pages/home/index',
-    'pages/cart/index',
-    'pages/user/index',
-    'pages/order/index',
-    'pages/pay/index',
-    'pages/detail/goods/index',
-    'pages/detail/logistics/index',
-    'pages/detail/order/index',
+    "pages/home/index",
+    "pages/cart/index",
+    "pages/user/index",
+    "pages/order/index",
+    "pages/pay/index",
+    "pages/detail/goods/index",
+    "pages/detail/logistics/index",
+    "pages/detail/order/index"
   ],
   window: {
-    backgroundTextStyle: 'light',
-    navigationBarBackgroundColor: '#fff',
-    navigationBarTitleText: 'WeChat',
-    navigationBarTextStyle: 'black'
+    backgroundTextStyle: "light",
+    navigationBarBackgroundColor: "#fff",
+    navigationBarTitleText: "WeChat",
+    navigationBarTextStyle: "black"
+  },
+  tabBar: {
+    color: "#999999",
+    selectedColor: "#F42121",
+    backgroundColor: "#FFFFFF",
+    list: [
+      {
+        pagePath: "pages/home/index",
+        text: "首页",
+        iconPath: "./public/icon/home.png",
+        selectedIconPath:  "./public/icon/home-select.png"
+      },
+      {
+        pagePath: "pages/cart/index",
+        text: "购物车",
+        iconPath: "./public/icon/cart.png",
+        selectedIconPath:  "./public/icon/cart-select.png"
+      },
+      {
+        pagePath: "pages/user/index",
+        text: "我的",
+        iconPath: "./public/icon/user.png",
+        selectedIconPath:  "./public/icon/user-select.png"
+      }
+    ],
+    position: "bottom"
   }
-})
+});
