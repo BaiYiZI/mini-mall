42cdc631486144a766076865fded6f506b1e279c
Zhangbokai614 <64344118+Zhangbokai614@users.noreply.github.com>
2022-09-18 15:36:35 +0800

    feat: Encapsulated requset

---
+++ src/config/service.js
@@ -0,0 +1,3 @@
+export const BaseUrl = "127.0.0.1:8080"
+
+export const OnConsole = process.env.NODE_ENV === 'development' ? true : false
---
+++ src/utils/request.js
@@ -0,0 +1,45 @@
+import Taro from "@tarojs/taro";
+import { options } from "@tarojs/taro-h5";
+import { BaseUrl, OnConsole } from "../config/service";
+
+export default async (url, data = {}, method = "GET") => {
+  const Dest = BaseUrl + url;
+
+  if (OnConsole) {
+    console.log(
+      `${new Date().toLocaleString()} dest: ${Dest} data: ${JSON.stringify(
+        options.data
+      )}`
+    );
+  }
+
+  return Taro.request({
+    url: Dest,
+    data,
+    header: {
+      "Content-Type": "application/json"
+    },
+    method: method
+  }).then(res => {
+    const { statusCode, data } = res;
+
+    if (statusCode == 200) {
+      if (OnConsole) {
+        console.log(
+          `${new Date().toLocaleString()} dest: ${Dest} res: ${JSON.stringify(
+            data
+          )}`
+        );
+      }
+
+      return data;
+    } else {
+      Taro.showToast({
+        title: "网络错误",
+        icon: "error"
+      });
+
+      return statusCode;
+    }
+  });
+};
