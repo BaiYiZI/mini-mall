6f27c7b84ed6119f3419cf4205823e1cfd644579
Zhangbokai614 <64344118+Zhangbokai614@users.noreply.github.com>
2022-09-20 14:03:18 +0800

    update: Home page add GoodsCard and Skeletons

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -2,13 +2,19 @@ import { Component } from "react";
 import Taro from "@tarojs/taro";
 import { View } from "@tarojs/components";
 
+import GoodsCard from "../../components/card/goods/index";
+
 import { getBanner } from "../../service/config";
+import { getGoodsInfo } from "../../service/goods";
 
 import style from "./index.module.less";
-import { Image } from "@taroify/core";
+import { Image, Flex, Skeleton, WhiteSpace } from "@taroify/core";
+
 export default class Index extends Component {
   state = {
-    banner: ""
+    banner: "",
+    goodsInfo: [],
+    loading: true
   };
 
   componentWillMount() {}
@@ -29,9 +35,12 @@ export default class Index extends Component {
     });
 
     const banner = await getBanner();
+    const goodsInfo = await getGoodsInfo();
 
     this.setState({
-      banner: banner[0]
+      banner: banner[0],
+      goodsInfo,
+      loading: false
     });
 
     Taro.hideLoading();
@@ -39,10 +48,43 @@ export default class Index extends Component {
 
   render() {
     let banner = this.state.banner;
+    let goodsList = this.state.goodsInfo.map((item, index) => {
+      return <GoodsCard title={item.title} img={item.img} price={item.price} />;
+    });
+    let skeletons = [];
+
+    for (let i = 0; i < 4; i++) {
+      skeletons.push(<WhiteSpace />);
+      skeletons.push(<Skeleton style={{ width: "90%" }} />);
+    }
 
     return (
       <View className="index">
-        <Image className={style["banner"]} src={banner} mode="hightFix" />
+        {this.state.loading ? (
+          <>
+            <Flex
+              direction="column"
+              justify="center"
+              align="center"
+              className={style["banner"]}
+            >
+              <WhiteSpace />
+              <Skeleton style={{ width: "100%", height: "90%" }} />
+              <WhiteSpace />
+            </Flex>
+
+            <Flex direction="column" justify="center" align="center">
+              {skeletons}
+              <WhiteSpace />
+              {skeletons}
+            </Flex>
+          </>
+        ) : (
+          <>
+            <Image className={style["banner"]} src={banner} mode="hightFix" />
+            <View className={style["goods-list"]}>{goodsList}</View>
+          </>
+        )}
       </View>
     );
   }
