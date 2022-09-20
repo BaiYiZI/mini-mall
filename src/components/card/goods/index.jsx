adaad37576b6bef179eed646ce61fe85e00ba2ff
Zhangbokai614 <64344118+Zhangbokai614@users.noreply.github.com>
2022-09-20 14:00:01 +0800

    feature: Add components GoodsCard

---
+++ src/components/card/goods/index.jsx
@@ -0,0 +1,50 @@
+import { Component } from "react";
+import { Text } from "@tarojs/components";
+
+import style from "./index.module.less";
+import { Flex, Image } from "@taroify/core";
+import { CartOutlined } from "@taroify/icons";
+
+export default class GoodsCard extends Component {
+  state = {
+    img: this.props.img,
+    title: this.props.title,
+    price: this.props.price,
+  };
+
+  render() {
+    return (
+      <Flex direction="column" gutter="24" className={style.root}>
+        <Flex.Item
+          justify="center"
+          align="center"
+          span="24"
+          className={style.content}
+        >
+          <Image src={this.state.img} mode="center" className={style.img} />
+        </Flex.Item>
+
+        <Flex className={style.content}>
+          <Flex.Item span="22">
+            <Text className={style["p-title"]}>{this.state.title}</Text>
+          </Flex.Item>
+        </Flex>
+
+        <Flex
+          direction="row"
+          justify="space-between"
+          align="center"
+          className={style.content}
+        >
+          <Flex.Item className={style["p-price"]} span="6">
+            <Text>{"￥" + this.state.price}</Text>
+          </Flex.Item>
+
+          <Flex.Item align="center" span="3" className={[style.testing]}>
+            <CartOutlined style={{ color: style["main-color"] }} size="24px" />
+          </Flex.Item>
+        </Flex>
+      </Flex>
+    );
+  }
+}
