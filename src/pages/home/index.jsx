32ed70ce8807c92b8fe2081d989f0e6c0c62c82a
Zhangbokai614 <64344118+Zhangbokai614@users.noreply.github.com>
2022-09-22 00:14:21 +0800

    feature: Add tabs bar and PullDownRefresh

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -4,55 +4,141 @@ import { View } from "@tarojs/components";
 
 import GoodsCard from "../../components/card/goods/index";
 
-import { getBanner } from "../../service/config";
-import { getGoodsInfo } from "../../service/goods";
+import { getBanner, getKinds } from "../../service/config";
+import { getGoodsInfoByKind } from "../../service/goods";
 
 import style from "./index.module.less";
-import { Image, Flex, Skeleton, WhiteSpace } from "@taroify/core";
+import {
+  Image,
+  Flex,
+  Skeleton,
+  WhiteSpace,
+  Tabs,
+  Backdrop,
+  Space,
+  Button,
+} from "@taroify/core";
 
 export default class Index extends Component {
   state = {
     banner: "",
     goodsInfo: [],
-    loading: true
+    loading: true,
+    kinds: [],
+    currentKind: "",
+    currentKindChildren: "",
+    open: false
   };
-
-  componentWillMount() {}
 
   componentDidMount() {
     this.loading();
   }
 
-  componentWillUnmount() {}
-
-  componentDidShow() {}
-
-  componentDidHide() {}
+  async onPullDownRefresh() {
+    Taro.startPullDownRefresh();
+    this.setState({
+      loading: true
+    });
+    await this.loading();
+    Taro.stopPullDownRefresh();
+  }
 
   async loading() {
     Taro.showLoading({
       title: "Loading"
     });
 
-    const banner = await getBanner();
-    const goodsInfo = await getGoodsInfo();
+    let banner = await getBanner();
+    let kinds = await getKinds();
+
+    let currentKind = kinds[0].kind;
+    let currentKindChildren = kinds[0].children[0];
+
+    let goodsInfo = await getGoodsInfoByKind(currentKindChildren);
 
     this.setState({
       banner: banner[0],
       goodsInfo,
+      kinds,
+      currentKind,
+      currentKindChildren,
       loading: false
     });
 
     Taro.hideLoading();
   }
 
-  render() {
-    let banner = this.state.banner;
-    let goodsList = this.state.goodsInfo.map((item, index) => {
-      return <GoodsCard title={item.title} img={item.img} price={item.price} />;
+  changeKind(value) {
+    this.setState({
+      currentKind: value
     });
-    let skeletons = [];
 
+    let children = this.state.kinds.filter(item => {
+      return item.kind == value;
+    })[0].children[0];
+    this.changeKindChildren(children);
+  }
+
+  async changeKindChildren(value) {
+    const goodsInfo = await getGoodsInfoByKind(value);
+
+    this.setState({
+      currentKindChildren: value,
+      goodsInfo
+    });
+  }
+
+  render() {
+    let loading = this.state.loading;
+    let banner = "";
+    let goodsList = [];
+    let tabs = [];
+    let kindButtons = [];
+
+    if (!loading) {
+      banner = this.state.banner;
+
+      tabs = this.state.kinds
+        .filter(item => {
+          return item.kind == this.state.currentKind;
+        })[0]
+        .children.map(item => {
+          return (
+            <Tabs.TabPane key={item} value={item} title={item}></Tabs.TabPane>
+          );
+        });
+
+      kindButtons = this.state.kinds
+        .filter(item => {
+          return item.kind != this.state.currentKind;
+        })
+        .map(item => {
+          return (
+            <Button
+              key={item.kind}
+              color=""
+              shape="round"
+              onClick={() => this.changeKind(item.kind)}
+              className={style["kind-button"]}
+            >
+              {item.kind}
+            </Button>
+          );
+        });
+
+      goodsList = this.state.goodsInfo.map(item => {
+        return (
+          <GoodsCard
+            key={item.title}
+            title={item.title}
+            img={item.img}
+            price={item.price}
+          />
+        );
+      });
+    }
+
+    let skeletons = [];
     for (let i = 0; i < 4; i++) {
       skeletons.push(<WhiteSpace />);
       skeletons.push(<Skeleton style={{ width: "90%" }} />);
@@ -82,6 +168,40 @@ export default class Index extends Component {
         ) : (
           <>
             <Image className={style["banner"]} src={banner} mode="hightFix" />
+            <Flex direction="row" align="center" className={style["tab-bar"]}>
+              <Flex.Item
+                span="5"
+                justify="center"
+                align="center"
+                className={[style["main-classif"], style["p-normal"]]}
+                onClick={() => this.setState({ open: true })}
+              >
+                {this.state.currentKind}
+              </Flex.Item>
+              <Flex.Item span="18">
+                <Tabs
+                  animated
+                  swipeable
+                  value={this.state.currentKindChildren}
+                  defaultValue={this.state.currentKindChildren}
+                  onChange={value => this.changeKindChildren(value)}
+                  style={{ height: "100%" }}
+                >
+                  {tabs}
+                </Tabs>
+              </Flex.Item>
+            </Flex>
+            <Backdrop
+              open={this.state.open}
+              closeable
+              onClose={() => this.setState({ open: false })}
+            >
+              <View className={style["content-wrapper"]}>
+                <Space className={style["content-block"]} size="small">
+                  {kindButtons}
+                </Space>
+              </View>
+            </Backdrop>
             <View className={style["goods-list"]}>{goodsList}</View>
           </>
         )}
